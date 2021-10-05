8ac6b0b39a100b71f609f3be473240570eb60a2b
eunbin <eunbines2@gmail.com>
2021-10-05 18:12:39 +0900

    Feat: create new plant

--- controllers/plants.controller.js
+++ controllers/plants.controller.js
@@ -1,12 +1,57 @@
 const Plant = require('../models/Plant');
-const { BaseError } = require('../lib/errors');
+const { BaseError, BadRequestError } = require('../lib/errors');
+const mongoose = require('mongoose');
 
 exports.getAllPlants = async (req, res, next) => {
   try {
     const plants = await Plant.find().lean();
 
     return res.status(201).json(plants);
-  } catch (err) {
+  } catch {
     next(new BaseError());
+  }
+};
+
+exports.createNewPlant = async (req, res, next) => {
+  const newPlant = req.body;
+
+  if (!newPlant) {
+    return next(new BadRequestError('식물 데이터가 유효하지 않습니다.'));
+  }
+
+  const {
+    name,
+    plantSpecies,
+    lightingEnvironment,
+    growthStage,
+    lastWindowOpen,
+    defaultSunGuage,
+    defaultWreringGuage,
+  } = newPlant;
+
+  try {
+    const plant = await Plant.create({
+      name,
+      plant_species: plantSpecies,
+      lighting_environment: lightingEnvironment,
+      growth_stage: growthStage,
+      last_window_open: lastWindowOpen,
+      sun_guage: {
+        default_guage: defaultSunGuage,
+      },
+      watering_guage: {
+        default_guage: defaultWreringGuage,
+      },
+    });
+
+    res.json({
+      plant,
+    });
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      return next(new BadRequestError(err.message));
+    }
+
+    next(BaseError(err.message));
   }
 };
