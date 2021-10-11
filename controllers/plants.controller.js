9bd889fca8199f7916c30e9de33d87a1338d412f
eunbin <eunbines2@gmail.com>
2021-10-11 23:17:19 +0900

    Feat: updatePlant controller, fix createNewPlant controller

--- controllers/plants.controller.js
+++ controllers/plants.controller.js
@@ -27,6 +27,8 @@ exports.getAllPlantsById = async (req, res, next) => {
 
 exports.createNewPlant = async (req, res, next) => {
   const newPlant = req.body;
+  const token = req.headers.authorization.split(' ')[1];
+  const { _id } = await jwt.verify(token, tokenSecretKey);
 
   if (!newPlant) {
     return next(new BadRequestError('식물 데이터가 유효하지 않습니다.'));
@@ -34,26 +36,27 @@ exports.createNewPlant = async (req, res, next) => {
 
   const {
     name,
-    plantSpecies,
-    lightingEnvironment,
+    type,
+    species,
+    isSunPlant,
     growthStage,
-    lastWindowOpen,
     defaultSunGuage,
-    defaultWreringGuage,
+    defaultWaterGuage,
   } = newPlant;
 
   try {
     const plant = await Plant.create({
+      userId: _id,
       name,
-      plant_species: plantSpecies,
-      lighting_environment: lightingEnvironment,
+      species,
+      type,
       growth_stage: growthStage,
-      last_window_open: lastWindowOpen,
+      is_sun_plant: isSunPlant,
       sun_guage: {
         default_guage: defaultSunGuage,
       },
       watering_guage: {
-        default_guage: defaultWreringGuage,
+        default_guage: defaultWaterGuage,
       },
     });
 
@@ -66,5 +69,76 @@ exports.createNewPlant = async (req, res, next) => {
     }
 
     next(BaseError(err.message));
+  }
+};
+
+exports.updatePlant = async (req, res, next) => {
+  try {
+    const { plantId } = req.params;
+    const { state, isIncrease } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(plantId)) {
+      return next(new BadRequestError());
+    }
+
+    const targetPlant = await Plant.findById(plantId).lean();
+
+    if (!targetPlant) {
+      return next(new BaseError());
+    }
+
+    switch (state) {
+      case 'water': {
+        if (isIncrease === true) {
+          targetPlant.water_guage.current_guage =
+            targetPlant.water_guage.current_guage + 1;
+        } else {
+          targetPlant.water_guage.current_guage =
+            targetPlant.water_guage.current_guage - 1;
+        }
+
+        break;
+      }
+
+      case 'sun': {
+        if (isIncrease === true) {
+          targetPlant.sun_guage.current_guage =
+            targetPlant.sun_guage.current_guage + 1;
+        } else {
+          targetPlant.sun_guage.current_guage =
+            targetPlant.sun_guage.current_guage - 1;
+        }
+
+        break;
+      }
+
+      case 'blind': {
+        targetPlant.is_blind_up = !targetPlant.is_blind_up;
+
+        break;
+      }
+
+      case 'penalty': {
+        if (isIncrease === true) {
+          targetPlant.penalty_points.current_guage =
+            targetPlant.penalty_points.current_guage + 1;
+        } else {
+          targetPlant.penalty_points.current_guage =
+            targetPlant.penalty_points.current_guage - 1;
+        }
+
+        break;
+      }
+    }
+
+    await targetPlant.save();
+
+    return res.status(201);
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      return next(new BadRequestError('유효하지 않은 데이터입니다.'));
+    }
+
+    next(new BaseError());
   }
 };
--- routes/plants.js
+++ routes/plants.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const plantsController = require('../controllers/plants.controller');
 
 router.get('/', plantsController.getAllPlants);
+router.post('/new', plantsController.createNewPlant);
+router.put('/:plantId', plantsController.updatePlant);
 
 module.exports = router;
