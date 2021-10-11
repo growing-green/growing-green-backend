73d0902bdfb7eb52df13b174210cabb2947deb03
eunbin <eunbines2@gmail.com>
2021-10-11 22:41:39 +0900

    Add: Plants model

--- models/Plant.js
+++ models/Plant.js
@@ -5,45 +5,6 @@ const schemaOption = {
   timestamps: true,
 };
 
-const plantSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Planr name is required'],
-    },
-    growth_stage: {
-      type: Number,
-      enum: [1, 2, 3],
-      required: [true, 'Growth stage is required'],
-    },
-    penalty_points: {
-      type: Number,
-      default: 0,
-      min: [0, 'Penalty points should be more than 1'],
-      max: [10, 'Penalty points should be less than 10'],
-    },
-    lighting_environment: {
-      type: String,
-      enum: {
-        value: ['sunny', 'shade'],
-        message: '{VALUE} is not supported',
-      },
-      required: [true, 'Lighting environment is required'],
-    },
-    last_window_open: {
-      type: Date,
-      default: null,
-    },
-    sun_guage: {
-      type: sunGuageSchema,
-    },
-    watering_guage: {
-      type: wateringGuageSchema,
-    },
-  },
-  schemaOption,
-);
-
 const sunGuageSchema = new mongoose.Schema({
   default_guage: {
     type: Number,
@@ -81,6 +42,60 @@ const wateringGuageSchema = new mongoose.Schema({
     default: null,
   },
 });
+
+const plantSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'User id is required'],
+    },
+    name: {
+      type: String,
+      required: [true, 'Plant name is required'],
+    },
+    species: {
+      type: String,
+      required: [true, 'Species is required'],
+    },
+    type: {
+      type: String,
+      required: [true, 'plant Type is required'],
+    },
+    isBlindUp: {
+      type: Boolean,
+      default: true,
+    },
+    growth_stage: {
+      type: Number,
+      enum: [1, 2, 3],
+      required: [true, 'Growth stage is required'],
+    },
+    penalty_points: {
+      type: Number,
+      default: 0,
+      min: [0, 'Penalty points should be more than 1'],
+      max: [10, 'Penalty points should be less than 10'],
+    },
+    is_sunplant: {
+      type: Boolean,
+      default: true,
+      required: [true, 'Lighting environment is required'],
+    },
+    last_window_open: {
+      type: Date,
+      default: null,
+    },
+    sun_guage: {
+      type: sunGuageSchema,
+    },
+    water_guage: {
+      type: wateringGuageSchema,
+    },
+  },
+  schemaOption,
+);
+
+plantSchema.index({ name: 1 }, { unique: true });
 
 plantSchema.post('save', function (error, doc, next) {
   if (error.name === 'MongoError' && error.code === 11000) {
