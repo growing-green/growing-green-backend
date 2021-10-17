b0fe6c1c16e45e5f2cf0d4ce02f5fc9391a20d16
eunbin <eunbines2@gmail.com>
2021-10-17 16:11:39 +0900

    Feat: add update all plant controller, delete plant controller

--- routes/plants.js
+++ routes/plants.js
@@ -5,5 +5,7 @@ const plantsController = require('../controllers/plants.controller');
 router.get('/', plantsController.getAllPlantsById);
 router.post('/new', plantsController.createNewPlant);
 router.put('/:plantId', plantsController.updatePlant);
+router.put('/', plantsController.updatePlantAll);
+router.delete('/:plantId', plantsController.deletePlant);
 
 module.exports = router;
